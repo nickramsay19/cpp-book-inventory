9c91b04d234d0466a0654b5c4fedb0585ad8cfab
nickramsay19 <nickramsay19@gmail.com>
2022-03-29 14:19:49 +1100

    added close bracket to Print output

--- src/Book.cpp
+++ src/Book.cpp
@@ -71,7 +71,7 @@ Book::Book(std::string bookTitle, std::string bookAuthor, std::initializer_list<
 void Book::Print() {
     printf("(\"%s\", \"%s\", [", title.c_str(), author.c_str());
     printGenres();
-    std::cout << "]\n";
+    std::cout << "])\n";
 }
 
 void Book::printGenres() {
